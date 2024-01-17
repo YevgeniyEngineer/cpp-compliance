c48244bcdf4d58dde9d25fd0f493d2b689aace32
YevgeniyEngineer <yevgeniy.engineer@gmail.com>
2024-01-17 23:41:32 +0800

    Testing various implementations

--- src/memcpy.cpp
+++ src/memcpy.cpp
@@ -1,10 +1,11 @@
-#include <chrono>   // std::chrono
-#include <cstddef>  // std::size_t
-#include <cstring>  // std::memcpy
-#include <iostream> // std::cout
-#include <random>   // std::mt19937
-#include <utility>  // std::index_sequence
-#include <vector>   // std::vector
+#include <chrono>    // std::chrono
+#include <cstddef>   // std::size_t
+#include <cstring>   // std::memcpy
+#include <exception> // std::terminate
+#include <iostream>  // std::cout
+#include <random>    // std::mt19937
+#include <utility>   // std::index_sequence
+#include <vector>    // std::vector
 
 extern "C"
 {
@@ -44,21 +45,61 @@ inline static void copyBlock(char *&dest, const char *&src, std::index_sequence<
     return dest;
 }
 
-[[maybe_unused]] static inline void *memcpySIMD(void *dest, const void *src, std::size_t n_bytes) noexcept
+// [[maybe_unused]] static inline void *memcpySIMD(void *dest, const void *src, std::size_t n_bytes) noexcept
+// {
+//     char *d = static_cast<char *>(dest);
+//     const char *s = static_cast<const char *>(src);
+
+//     static constexpr std::size_t BLOCK_SIZE = 16U; // SSE2 can handle 16 bytes at a time
+//     static constexpr std::size_t CONST = BLOCK_SIZE - 1U;
+
+//     const std::size_t unroll_count = n_bytes / BLOCK_SIZE;
+//     const std::size_t remaining_bytes = n_bytes & CONST;
+
+//     for (std::size_t i = 0U; i < unroll_count; ++i, s += BLOCK_SIZE, d += BLOCK_SIZE)
+//     {
+//         __m128i data = _mm_loadu_si128(reinterpret_cast<const __m128i *>(s));
+//         _mm_storeu_si128(reinterpret_cast<__m128i *>(d), data);
+//     }
+
+//     // Copy any remaining bytes
+//     for (std::size_t i = 0U; i < remaining_bytes; ++i)
+//     {
+//         d[i] = s[i];
+//     }
+
+//     return dest;
+// }
+
+[[maybe_unused]] static inline void *memcpySIMD(void *const __restrict__ dest, const void *const __restrict__ src,
+                                                std::size_t n_bytes) noexcept
 {
-    char *d = static_cast<char *>(dest);
-    const char *s = static_cast<const char *>(src);
+    auto *const d = static_cast<std::uint8_t *const>(dest);
+    const auto *const s = static_cast<const std::uint8_t *const>(src);
 
     static constexpr std::size_t BLOCK_SIZE = 16U; // SSE2 can handle 16 bytes at a time
     static constexpr std::size_t CONST = BLOCK_SIZE - 1U;
 
-    const std::size_t unroll_count = n_bytes / BLOCK_SIZE;
+    const std::size_t unroll_count = n_bytes / (BLOCK_SIZE * 4); // Unroll by 4
     const std::size_t remaining_bytes = n_bytes & CONST;
 
-    for (std::size_t i = 0U; i < unroll_count; ++i, s += BLOCK_SIZE, d += BLOCK_SIZE)
+    // Ensure proper alignment
+    __m128i *d_aligned = reinterpret_cast<__m128i *>(d);
+    const __m128i *s_aligned = reinterpret_cast<const __m128i *>(s);
+
+    // Prefetch the data
+    _mm_prefetch(reinterpret_cast<const std::uint8_t *const>(s_aligned), _MM_HINT_T0);
+
+    // Unroll the loop by 4
+    for (std::size_t i = 0U; i < unroll_count; ++i, s_aligned += 4, d_aligned += 4)
     {
-        __m128i data = _mm_loadu_si128(reinterpret_cast<const __m128i *>(s));
-        _mm_storeu_si128(reinterpret_cast<__m128i *>(d), data);
+        _mm_store_si128(d_aligned, _mm_load_si128(s_aligned));
+        _mm_store_si128(d_aligned + 1, _mm_load_si128(s_aligned + 1));
+        _mm_store_si128(d_aligned + 2, _mm_load_si128(s_aligned + 2));
+        _mm_store_si128(d_aligned + 3, _mm_load_si128(s_aligned + 3));
+
+        // Prefetch the next data
+        _mm_prefetch(reinterpret_cast<const std::uint8_t *const>(s_aligned + 4), _MM_HINT_T0);
     }
 
     // Copy any remaining bytes
@@ -184,11 +225,64 @@ template <typename DestinationType, typename SourceType, std::size_t BlockSize>
 
     return dest;
 }
+
+[[maybe_unused]] static inline void *memcpySimple(void *const __restrict__ dest, const void *const __restrict__ src,
+                                                  const std::size_t n_bytes) noexcept
+{
+    // Check for nullpointers
+    if ((nullptr != dest) && (nullptr != src))
+    {
+        auto *const d = reinterpret_cast<std::uint8_t *const>(dest);
+        const auto *const s = reinterpret_cast<const std::uint8_t *const>(src);
+
+        // Check for buffer overlap
+        if (((d >= s) && (d <= (s + n_bytes))) || ((s >= d) && (s <= (d + n_bytes))))
+        {
+            std::terminate();
+        }
+
+        // Copy bytes
+        for (std::size_t i = 0U; i < n_bytes; ++i)
+        {
+            d[i] = s[i];
+        }
+    }
+
+    return dest;
+}
+
+template <typename DestinationType, typename SourceType>
+[[maybe_unused]] static inline void *memcpySimpleTemplated(DestinationType *const __restrict__ dest,
+                                                           const SourceType *const __restrict__ src,
+                                                           const std::size_t n_bytes) noexcept
+{
+    // Check for nullpointers
+    if ((nullptr != dest) && (nullptr != src))
+    {
+        auto *const d = reinterpret_cast<std::uint8_t *const>(dest);
+        const auto *const s = reinterpret_cast<const std::uint8_t *const>(src);
+
+        // Check for buffer overlap
+        if (((d >= s) && (d <= (s + n_bytes))) || ((s >= d) && (s <= (d + n_bytes))))
+        {
+            std::terminate();
+        }
+
+        // Copy bytes
+        for (std::size_t i = 0U; i < n_bytes; ++i)
+        {
+            d[i] = s[i];
+        }
+    }
+
+    return dest;
+}
+
 } // namespace nostd
 
 int main()
 {
-    static constexpr std::size_t NUM_FLOATS = 400'000U;
+    static constexpr std::size_t NUM_FLOATS = 40'000U;
     static constexpr std::size_t NUM_BYTES = NUM_FLOATS * sizeof(float);
 
     // Creating test data
@@ -215,49 +309,55 @@ int main()
         end = std::chrono::high_resolution_clock::now();
         std::cout << "std::memcpy took: " << std::chrono::duration_cast<std::chrono::microseconds>(end - start).count()
                   << " microseconds." << std::endl;
-    }
 
-    // // Time nostd::memcpyUint64
-    // start = std::chrono::high_resolution_clock::now();
-    // nostd::memcpyUint64(dest_nostd.data(), src.data(), NUM_BYTES);
-    // end = std::chrono::high_resolution_clock::now();
-    // std::cout << "nostd::memcpyUint64 took: "
-    //           << std::chrono::duration_cast<std::chrono::microseconds>(end - start).count() << " microseconds."
-    //           << std::endl;
+        // Time nostd::memcpyUint64
+        start = std::chrono::high_resolution_clock::now();
+        nostd::memcpyUint64(dest_nostd.data(), src.data(), NUM_BYTES);
+        end = std::chrono::high_resolution_clock::now();
+        std::cout << "nostd::memcpyUint64 took: "
+                  << std::chrono::duration_cast<std::chrono::microseconds>(end - start).count() << " microseconds."
+                  << std::endl;
 
-    // // Time nostd::memcpyUnrolled
-    // start = std::chrono::high_resolution_clock::now();
-    // nostd::memcpyUnrolled(dest_nostd.data(), src.data(), NUM_BYTES);
-    // end = std::chrono::high_resolution_clock::now();
-    // std::cout << "nostd::memcpyUnrolled took: "
-    //           << std::chrono::duration_cast<std::chrono::microseconds>(end - start).count() << " microseconds."
-    //           << std::endl;
+        // Time nostd::memcpyUnrolled
+        start = std::chrono::high_resolution_clock::now();
+        nostd::memcpyUnrolled(dest_nostd.data(), src.data(), NUM_BYTES);
+        end = std::chrono::high_resolution_clock::now();
+        std::cout << "nostd::memcpyUnrolled took: "
+                  << std::chrono::duration_cast<std::chrono::microseconds>(end - start).count() << " microseconds."
+                  << std::endl;
 
-    // // Time nostd::memcpySIMD
-    // start = std::chrono::high_resolution_clock::now();
-    // nostd::memcpySIMD(dest_nostd.data(), src.data(), NUM_BYTES);
-    // end = std::chrono::high_resolution_clock::now();
-    // std::cout << "nostd::memcpySIMD took: "
-    //           << std::chrono::duration_cast<std::chrono::microseconds>(end - start).count() << " microseconds."
-    //           << std::endl;
+        // Time nostd::memcpySIMD
+        start = std::chrono::high_resolution_clock::now();
+        nostd::memcpySIMD(dest_nostd.data(), src.data(), NUM_BYTES);
+        end = std::chrono::high_resolution_clock::now();
+        std::cout << "nostd::memcpySIMD took: "
+                  << std::chrono::duration_cast<std::chrono::microseconds>(end - start).count() << " microseconds."
+                  << std::endl;
 
-    // // Time nostd::memcpySIMDAndUnroll
-    // start = std::chrono::high_resolution_clock::now();
-    // nostd::memcpySIMDAndUnroll(dest_nostd.data(), src.data(), NUM_BYTES);
-    // end = std::chrono::high_resolution_clock::now();
-    // std::cout << "nostd::memcpySIMDAndUnroll took: "
-    //           << std::chrono::duration_cast<std::chrono::microseconds>(end - start).count() << " microseconds."
-    //           << std::endl;
+        // Time nostd::memcpySIMDAndUnroll
+        start = std::chrono::high_resolution_clock::now();
+        nostd::memcpySIMDAndUnroll(dest_nostd.data(), src.data(), NUM_BYTES);
+        end = std::chrono::high_resolution_clock::now();
+        std::cout << "nostd::memcpySIMDAndUnroll took: "
+                  << std::chrono::duration_cast<std::chrono::microseconds>(end - start).count() << " microseconds."
+                  << std::endl;
 
-    // Time nostd::memcpy
-    for (int i = 0; i < 100; ++i)
-    {
+        // Time nostd::memcpy
         start = std::chrono::high_resolution_clock::now();
         nostd::memcpy<float, char, sizeof(float)>(dest_nostd.data(), src.data(), NUM_BYTES);
         end = std::chrono::high_resolution_clock::now();
         std::cout << "nostd::memcpy took: "
                   << std::chrono::duration_cast<std::chrono::microseconds>(end - start).count() << " microseconds."
                   << std::endl;
+
+        start = std::chrono::high_resolution_clock::now();
+        nostd::memcpySimpleTemplated(dest_nostd.data(), src.data(), NUM_BYTES);
+        end = std::chrono::high_resolution_clock::now();
+        std::cout << "nostd::memcpySimple took: "
+                  << std::chrono::duration_cast<std::chrono::microseconds>(end - start).count() << " microseconds."
+                  << std::endl;
+
+        std::cout << std::endl;
     }
 
     return 0;
